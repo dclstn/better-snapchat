97473b2f71840e53e2142a3d3298376de434c5f8
dclstn <donnieacolston@gmail.com>
2022-09-19 19:19:07 +0100

    inject script

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,10 +1,10 @@
-const ESBuild = require("esbuild");
+const ESBuild = require('esbuild');
 
 ESBuild.buildSync({
-  entryPoints: ["./src/index.js"],
+  entryPoints: ['./src'],
   bundle: true,
   minify: true,
   sourcemap: true,
-  target: ["chrome58", "firefox57", "safari11", "edge16"],
-  outfile: "./dist/index.js",
+  target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+  outfile: './dist/index.js',
 });
