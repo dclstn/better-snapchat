35db6a3c4d77a189d62956b00dc02c593ca36ed7
Donnie <donnieacolston@gmail.com>
2024-03-14 00:58:17 +0000

    update user script to circumvent CSP

--- inject.user.js
+++ inject.user.js
@@ -1,21 +1,24 @@
 // ==UserScript==
 // @name         Better Snapchat
-// @version      1.0.0
+// @version      1.2.0
 // @description  This browser extension enhances your web.snapchat.com experience by adding several essential privacy features.
 // @author       VaspDev
 // @match        https://web.snapchat.com/*
 // @icon         https://better-snapchat.pages.dev/logo128.png
-// @grant        GM_xmlhttpRequest
+// @grant        GM_addElement
 // @run-at       document-start
 // @connect      better-snapchat.vasp.dev
 // ==/UserScript==
 
-GM_xmlhttpRequest({
-  method: 'GET',
-  url: 'https://better-snapchat.vasp.dev/build/script.js',
-  onload: (response) => {
-    const element = document.createElement('script');
-    element.innerText = response.responseText;
-    document.head.appendChild(element);
-  },
+const head = document.querySelector('head');
+
+GM_addElement(head, 'script', {
+  src: 'https://better-snapchat.vasp.dev/build/script.js',
+  type: 'text/javascript',
+});
+
+GM_addElement(head, 'link', {
+  rel: 'stylesheet',
+  href: 'https://better-snapchat.vasp.dev/build/script.css',
+  type: 'text/css',
 });
