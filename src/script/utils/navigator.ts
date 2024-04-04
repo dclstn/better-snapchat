748bb8a2163a8be317fa184ba2bb1ed9b81922eb
Donnie <donnieacolston@gmail.com>
2024-04-05 00:03:32 +0100

    remove broken user agent

--- src/script/utils/navigator.ts
+++ src/script/utils/navigator.ts
@@ -1,7 +1,7 @@
+// TODO: Add more user agents
 const USER_AGENTS = [
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-  'Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Safari/605.1.15',
 ];
 
 function patchUserAgent() {
