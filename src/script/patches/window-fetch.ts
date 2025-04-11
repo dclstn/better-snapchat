a49f80f8acb5db06a22c9eec7a612294104e719d
Donnie <donnieacolston@gmail.com>
2025-04-11 15:44:20 +0100

    fix story read receipt

--- src/script/patches/window-fetch.ts
+++ src/script/patches/window-fetch.ts
@@ -1,17 +1,17 @@
 import Patch from '../lib/patch';
 import settings from '../lib/settings';
 
-const STORY_READ_RECEIPT_URL = 'https://web.snapchat.com/readreceipt-indexer/batchuploadreadreceipts';
+const STORY_READ_RECEIPT_REGEX = /\/readreceipt-indexer\/batchuploadreadreceipts/;
 
-class StoryReadFetch extends Patch {
+class WindowFetch extends Patch {
   constructor() {
-    super('StoryReadFetch');
+    super('WindowFetch');
   }
 
   patch() {
     window.fetch = new Proxy(window.fetch, {
       apply(target, thisArg, [request, ...rest]: [Request, AbortSignal]) {
-        if (settings.getSetting('PREVENT_STORY_READ_RECEIPTS') && request.url?.startsWith(STORY_READ_RECEIPT_URL)) {
+        if (settings.getSetting('PREVENT_STORY_READ_RECEIPTS') && STORY_READ_RECEIPT_REGEX.test(request.url)) {
           return new Promise((resolve) => resolve(new Response(null, { status: 200 })));
         }
         return Reflect.apply(target, thisArg, [request, ...rest]);
@@ -20,4 +20,4 @@ class StoryReadFetch extends Patch {
   }
 }
 
-export default new StoryReadFetch();
+export default new WindowFetch();
