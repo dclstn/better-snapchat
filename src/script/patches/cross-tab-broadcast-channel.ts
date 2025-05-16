ba4889cc777c5c79554016ac5dd8f0e3f37a41e1
Donnie <donnieacolston@gmail.com>
2025-05-16 18:39:10 +0100

    repair broadcast channel when patched

--- src/script/patches/cross-tab-broadcast-channel.ts
+++ src/script/patches/cross-tab-broadcast-channel.ts
@@ -1,20 +1,32 @@
 import Patch from '../lib/patch';
 import settings from '../lib/settings';
 
+const CROSS_TAB_BROADCAST_CHANNEL = 'cross_tab';
+
 class CrossTabBroadcastChannel extends Patch {
   constructor() {
     super('Cross-Tab Broadcast Channel');
   }
 
   patch() {
+    const oldBroadcastChannel = window.BroadcastChannel;
+
     window.BroadcastChannel = class PatchedBroadcastChannel extends window.BroadcastChannel {
+      constructor(name: string) {
+        super(name);
+
+        if (name === CROSS_TAB_BROADCAST_CHANNEL) {
+          window.BroadcastChannel = oldBroadcastChannel;
+        }
+      }
+
       addEventListener(type: string, listener: EventListener) {
+        if (this.name !== CROSS_TAB_BROADCAST_CHANNEL) {
+          return super.addEventListener(type, listener);
+        }
+
         return super.addEventListener(type, ((event: MessageEvent) => {
-          if (
-            this.name === 'cross_tab' &&
-            settings.getSetting('ALLOW_CROSS_TAB') &&
-            event.data.type === 'CLAIM_ACTIVE'
-          ) {
+          if (settings.getSetting('ALLOW_CROSS_TAB') && event.data.type === 'CLAIM_ACTIVE') {
             return;
           }
 
